0a5433f35e6b3119a97683be614dff27ff5d0172
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-06-30 20:07:32 +0500

    completing topbar

--- components/App-Shell/Topbar.js
+++ components/App-Shell/Topbar.js
@@ -2,9 +2,12 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { Colors } from "../../theme/ColorScheme";
 import { useEffect, useState } from "react";
 import * as SecureStore from "expo-secure-store";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Topbar() {
   const API_URL = "http://192.168.100.77:5009";
+  const navigation = useNavigation();
   const [user, setUser] = useState("Guest");
 
   useEffect(() => {
@@ -18,6 +21,7 @@ export default function Topbar() {
         });
 
         const data = await res.json();
+
         if (data.success) {
           setUser(data.user);
         }
@@ -25,25 +29,48 @@ export default function Topbar() {
         console.error(err);
       }
     };
+
     fetchUser();
   }, []);
 
-const userInitials = () => {
-  return user?.name
-    ? user.name.split(" ").map(word => word[0]).join("").toUpperCase()
-    : "G";
-};
+  const userInitials = () => {
+    return user?.name
+      ? user.name
+          .split(" ")
+          .map((word) => word[0])
+          .join("")
+          .toUpperCase()
+      : "G";
+  };
 
   return (
-    <>
-      <View className="flex-1 justify-between items-start mt-10">
-        <TouchableOpacity
-          className="ml-3"
-          style={{ backgroundColor: Colors.surface }}
-        >
-          <Text className="text-white">{userInitials()}</Text>
-        </TouchableOpacity>
-      </View>
-    </>
+    <View className="flex-row justify-between items-center mt-12 px-3">
+      <TouchableOpacity
+        className="items-center justify-center"
+        style={{
+          backgroundColor: Colors.card,
+          width: 40,
+          height: 40,
+          borderRadius: 20,
+        }}
+        onPress={() => navigation.navigate("UserProfileScreen")}
+      >
+        <Text className="text-white font-bold text-[18px]">
+          {userInitials()}
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        className="items-center justify-center"
+        style={{
+          backgroundColor: Colors.card,
+          width: 40,
+          height: 40,
+          borderRadius: 20,
+        }}
+      >
+        <Ionicons name="settings-outline" size={22} color="white" />
+      </TouchableOpacity>
+    </View>
   );
 }
